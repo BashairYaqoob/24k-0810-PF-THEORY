381a9f12054c4a4e973fa3bb28753615883a0c3e
bashair yaqoob <bashairyaqoob@gmail.com>
2024-10-26 16:41:08 +0500

    Update Q1.c

--- Theory assignment/Assignment 2/Q1/Q1.c
+++ Theory assignment/Assignment 2/Q1/Q1.c
@@ -1,1 +1,33 @@
+/* *Bashair Yaqoob
+24K-0810, 26-10-2024
+Second smallest element in an array.*/
 
+#include <stdio.h>
+int main(){
+int i, n, small, secsmall;
+printf("Enter number of elements you want to enter: ");
+scanf("%d", &n);
+if(n<2){
+printf("\nArray must have atleast 2 numbers.");
+return 0;}
+int arr[n];
+for(i=0; i<n; i++){
+printf("\nElement %d: ",i);
+scanf("%d", &arr[i]);
+}
+if(arr[0]<arr[1]){
+small=arr[0]; secsmall=arr[1];}
+else{
+small=arr[1]; secsmall=arr[0];}
+for(i=2; i<n; i++){
+if(arr[i]<small){
+secsmall=small;
+small=arr[i];
+}
+else if(arr[i]<secsmall && arr[i] != small){
+secsmall = arr[i];
+}
+}
+printf("Second smallest number is: %d", secsmall);
+return 0;
+}
